30a0e3d02229b88d8348ee62b5256915ea7f7755
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-16 03:04:50 +0530

    Update test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -63,7 +63,7 @@ if(!c === 'SD 480p' && !c === 'HD 720p' && !c === 'FHD 1080p' && !c === '4K 2160
       
 }catch(e){
 console.log(e)
-conn.sendMessage( `94771709545@s.whatsapp.net` , {text: `${e}` }, {quoted: mek})
+await conn.sendMessage( `94771709545@s.whatsapp.net` , {text: `${e}` }, {quoted: mek})
 
 }
 })
