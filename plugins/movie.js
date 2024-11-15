bba288c7e2b67a652fb7eb9d3b85ea419ec9f650
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-15 21:15:32 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -132,6 +132,17 @@ cmd({
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
 
+        let x = q
+        
+let a = x.split(" & ")
+
+let b = a[0]
+let c = a[1]
+
+
+if(!c) {
+
+
 let id = config.MV_SEND_JID
 
 if (!q && !q.startsWith("https://sinhalasub.lk/")) {
@@ -188,6 +199,64 @@ let msg2 = `🍟 *${mv_info2.result.title}*
 
 await conn.sendMessage(id ,{image:{url: mv_info2.result.images[0]},caption:msg2})
 
+}
+} else if (c.endsWith('@g.us')) {
+
+if (!q && !q.startsWith("https://sinhalasub.lk/")) {
+
+return reply("*_Please give me a movie name or sinhalasub.lk url._*")
+
+} else if (q.startsWith("https://")) {
+
+let mv_info = await SinhalaSub.movie(q)
+
+let msg = `🍟 *${mv_info.result.title}*
+
+🧿 *Release Date :* ${mv_info.result.release_date}
+
+🌍 *Country :* ${mv_info.result.country}
+
+⏱ *Duration :* ${mv_info.result.duration}
+
+🎀 *Categories :* ${mv_info.result.categories}
+
+⭐ *IMDB Rate :* ${mv_info.result.IMDb_Rating}
+
+🤵‍♂ *Director* : ${mv_info.result.director.name}
+
+🖇️ *Link* : ${q}
+
+> ɪɴꜰɪɴɪᴛʏ ᴍᴏᴠɪᴇ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+await conn.sendMessage( c ,{image:{url: mv_info.result.images[0]},caption:msg})
+
+} else {
+
+let mv = await SinhalaSub.get_list.by_search(q)
+
+let mv_info2 = await SinhalaSub.movie(mv.results[0].link)
+
+let msg2 = `🍟 *${mv_info2.result.title}*
+
+🧿 *Release Date :* ${mv_info2.result.release_date}
+
+🌍 *Country :* ${mv_info2.result.country}
+
+⏱ *Duration :* ${mv_info2.result.duration}
+
+🎀 *Categories :* ${mv_info2.result.categories}
+
+⭐ *IMDB Rate :* ${mv_info2.result.IMDb_Rating}
+
+🤵‍♂ *Director* : ${mv_info2.result.director.name}
+
+🖇️ *Link* : ${mv.results[0].link}
+
+> ɪɴꜰɪɴɪᴛʏ ᴍᴏᴠɪᴇ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+await conn.sendMessage( c ,{image:{url: mv_info2.result.images[0]},caption:msg2})
+
+}
 }
     
 }catch(e){
