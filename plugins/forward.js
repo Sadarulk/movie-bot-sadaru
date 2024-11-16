44d3bc2965ce8cf67bb0af02fa2a4c7d676afde0
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-16 20:46:45 +0530

    Update and rename test3.js to forward.js

--- plugins/forward.js
+++ plugins/forward.js
@@ -2,22 +2,24 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 
 cmd({
-    pattern: "test",
-    desc: "test",
+    pattern: "forward",
+    alias: ["fd"],
+    desc: "forward a msg",
     category: "other",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-
+    if(!q) return reply(`*_please give me a jid to forward this_*`)
+    
 if(m.quoted.type === 'imageMessage') {
 
     var nameJpg = `testMsg`
     
       const image = await m.quoted.download(nameJpg)
     
-      await conn.sendMessage('94701814946@s.whatsapp.net', { image : image })
+      await conn.sendMessage(q, { image : image })
     
 } else if(m.quoted.type === 'videoMessage') {
 
@@ -25,7 +27,7 @@ if(m.quoted.type === 'imageMessage') {
     
       const video = await m.quoted.download(nameVid)
     
-      await conn.sendMessage('94701814946@s.whatsapp.net', { video : video , mimetype:"video/mp4"})
+      await conn.sendMessage(q, { video : video , mimetype:"video/mp4"})
     
 }  else if(m.quoted.type === 'audioMessage') {
 
@@ -33,27 +35,29 @@ if(m.quoted.type === 'imageMessage') {
     
       const audio = await m.quoted.download(nameAud)
     
-      await conn.sendMessage('94701814946@s.whatsapp.net', { audio : audio , mimetype:"audio/mpeg"})
-
-} else if (m.quoted.type === 'documentMessage') {
-
-    await conn.sendMessage('94701814946@s.whatsapp.net',{ text : m.msg.fileName })
+      await conn.sendMessage(q, { audio : audio , mimetype:"audio/mpeg"})
     
 } else if (m.quoted.type === 'stickerMessage') {
 
-    var nameStik = `testMsg4`
+    var nameStik = `testMsg3`
     
       const sticker = await m.quoted.download(nameStik)
     
-      await conn.sendMessage('94701814946@s.whatsapp.net', { sticker : sticker })
+      await conn.sendMessage(q, { sticker : sticker })
+    
+    } else if (m.quoted.type === 'documentMessage') {
+
+    return reply(`*_Can't forward this media_*`)
     
 } else {
 
     var msg = m.quoted.msg
     
-    await conn.sendMessage('94701814946@s.whatsapp.net', { text : msg })
+    await conn.sendMessage(q, { text : msg })
     
 }
+
+   
 
 }catch(e){
 console.log(e)
