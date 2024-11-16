6773842a9e85c2df2c73355df7de277c34c46e84
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-16 22:21:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID || "32wQBC7B#HyshEG-YRnN6KZOSf3a_iuSanPbrk2uv
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/infinitybotlogo.png?raw=true",
 MV_SEND_JID: process.env.MV_SEND_JID || "120363292156762185@g.us",
 MODE: process.env.MODE || "public",
+AUTO_AI: process.env.AUTO_AI || "on", // on or off
 };
