9a0407b1892bd31a65fbd8b77096d86abeabe1fe
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-16 22:39:56 +0530

    Update and rename test.js to autoai.js

--- plugins/autoai.js
+++ plugins/autoai.js
@@ -8,15 +8,20 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
+const rejectGrp = ['120363355439809658@g.us', '120363359103844355@g.us']
+    
 if(isCmd) return
-if(senderNumber === botNumber) return   
+if(senderNumber === botNumber) return      
+if(rejectGrp.includes(from)) return
 
 if (config.AUTO_AI === 'on') {
     
-let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${body}`)
+let data = await fetchJson(`https://dark-yasiya-api-new.vercel.app/ai/chatgpt?q=${body}`)
 
-return reply(`${data.data}\n\n> ɪɴꜰɪɴɪᴛʏ ᴍᴏᴠɪᴇ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`)
+return reply(`${data.result}`)
+    
 }
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
