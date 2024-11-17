0a35d07087819e3bb74b1e9040086a55223076ce
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-17 10:06:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,6 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sadarulk/QueenMatheeDB/b
 MV_SEND_JID: process.env.MV_SEND_JID || "120363355439809658@g.us",
 MODE: process.env.MODE || "public",
 AUTO_AI: process.env.AUTO_AI || "on", // on or off
-AUTO_AI_JID: process.env.AUTO_AI_JID || ["120363365489430422@g.us","120363367074890944@g.us"],
+AUTO_AI_JID: process.env.AUTO_AI_JID || "120363365489430422@g.us",
 BLOCK_JID: process.env.BLOCK_JID || "120363365489430422@g.us",
 };
