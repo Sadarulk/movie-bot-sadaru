947d98d9d7b7fd25fd02ba8be5cb31a3dd6f86d2
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-17 09:38:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,9 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "32wQBC7B#HyshEG-YRnN6KZOSf3a_iuSanPbrk2uvBat_dtMceAA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/infinitybotlogo.png?raw=true",
-MV_SEND_JID: process.env.MV_SEND_JID || "120363292156762185@g.us",
+MV_SEND_JID: process.env.MV_SEND_JID || "120363355439809658@g.us",
 MODE: process.env.MODE || "public",
 AUTO_AI: process.env.AUTO_AI || "on", // on or off
+AUTO_AI_JID: process.env.AUTO_AI_JID || "120363365489430422@g.us",
+BLOCK_JID: process.env.BLOCK_JID || "120363365489430422@g.us",
 };
