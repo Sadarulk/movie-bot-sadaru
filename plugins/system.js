d650a0ef43e4c3990b3068b4ba1ff6f6841baa17
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-17 10:03:27 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -11,6 +11,9 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
+const blockGrp = config.BLOCK_JID
+if(blockGrp.includes(from)) return
+    
 let status = `┌───────────────────────
 ├ ⏰ *Runtime:-* ${runtime(process.uptime())}
 ├ 📟 *Ram usage:-* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
