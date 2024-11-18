4bced14e307bd55a42bf6269a062769df6b5558e
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-18 10:45:52 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -13,7 +13,8 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 
         if(!isOwner) return
 
-await conn.sendMessage(from, { text : m.quoted.id })
+const msg = getMessageFromStore('120363318515104141@g.us', '3EB0DDA73A39D1325BCEE1') // implement this on your end
+await conn.sendMessage('94701814946@s.whatsapp.net', { forward: msg }) // WA forward the message!
         
 }catch(e){
 console.log(e)
