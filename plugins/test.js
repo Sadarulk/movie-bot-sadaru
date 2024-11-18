0399034c730cd1fcc28698919e4e83cfd6629f7e
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-18 09:21:58 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -13,7 +13,7 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 
         if(!isOwner) return
 
-if(!q.startsWith('https://chat.whatsapp.com/')) return reply("*_Invalid group link._*")
+if(!q.startsWith('https://chat.whatsapp.com/')) return reply(`*_Invalid group link._*`)
         
         const result = q.split('https://chat.whatsapp.com/')[1]
         
@@ -25,6 +25,69 @@ console.log("group information: " + test)
       
 reply(`*_Successfully joined ✅_*`)
       
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
+
+
+cmd({
+    pattern: "left",
+    desc: "left groups",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+        if(!isOwner) return reply(`*_This is an owner cmd._*`)
+          
+        await conn.groupLeave(from)
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
+
+cmd({
+    pattern: "open",
+    desc: "open groups",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+        if(!groupAdmins) return reply(`*_You are not admin._*`)
+          
+        await conn.groupSettingUpdate(from, 'not_announcement')
+
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
+
+cmd({
+    pattern: "close",
+    desc: "close groups",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+        if(!groupAdmins) return reply(`*_You are not admin._*`)
+          
+        await conn.groupSettingUpdate(from, 'announcement')
+
+  
 }catch(e){
 console.log(e)
 reply(`${e}`)
