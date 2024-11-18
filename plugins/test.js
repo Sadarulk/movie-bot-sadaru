2cb2715304351e05e9f6d961f7ba26771223e358
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-18 11:01:18 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
 
-       await conn.sendMessage(from, {text : `Hello test`}, contextInfo: { forwardingScore: 999, isForwarded: true })
+       await conn.sendMessage(from, {text : `Hello test`}, {contextInfo: { forwardingScore: 999, isForwarded: true }})
         
 }catch(e){
 console.log(e)
