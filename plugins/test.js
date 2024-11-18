132cca525ec664e13ee725c452b4dddbdc4a1dd4
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-18 11:46:18 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -2,7 +2,7 @@ const { cmd, commands } = require('../command')
 const config = require('../config')
 
 cmd({
-    pattern: "del",
+    pattern: "mathee",
     desc: "test",
     category: "owner",
     filename: __filename
@@ -12,50 +12,16 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 
 if(!isOwner) return
 
-let response = await conn.sendMessage(from, { text: 'hello!' })
-        
-let a = await conn.sendMessage(from, {
-      text: '▄',
+setTimeout(() => {
+    
+  let response = await conn.sendMessage(from, { text: 'සුරංගනාවියක්' })
+    
+}, 5000);
+
+        await conn.sendMessage(from, {
+      text: 'හැබැයි බඩුවක්',
       edit: response.key,
     });
-let b = await conn.sendMessage(from, {
-      text: '▄▅',
-      edit: a.key,
-    });        
-let c = await conn.sendMessage(from, {
-      text: '▄▅▆',
-      edit: b.key,
-    });        
-let d = await conn.sendMessage(from, {
-      text: '▄▅▆▇',
-      edit: c.key,
-    });        
-let f = await conn.sendMessage(from, {
-      text: '▄▅▆▇█',
-      edit: d.key,
-    });        
-let g = await conn.sendMessage(from, {
-      text: '▄▅▆▇██',
-      edit: f.key,
-    });
-let h = await conn.sendMessage(from, {
-      text: '▄▅▆▇██▇',
-      edit: g.key,
-    });        
-let i = await conn.sendMessage(from, {
-      text: '▄▅▆▇██▇▆',
-      edit: h.key,
-    });        
-let j = await conn.sendMessage(from, {
-      text: '▄▅▆▇██▇▆▅',
-      edit: i.key,
-    });        
-let k = await conn.sendMessage(from, {
-      text: '▄▅▆▇██▇▆▅▄',
-      edit: j.key,
-    });
-
-
         
 }catch(e){
 console.log(e)
