924d6f1928a97b2d5a4cd648dc01d46c3a287abd
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-19 19:41:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "32wQBC7B#HyshEG-YRnN6KZOSf3a_iuSanPbrk2uvBat_dtMceAA",
-MV_SEND_JID: process.env.MV_SEND_JID || "120363355439809658@g.us",
-MODE: process.env.MODE || "public",
-AUTO_AI: process.env.AUTO_AI || "on", // on or off
-AUTO_AI_JID: process.env.AUTO_AI_JID || "120363365489430422@g.us",
-BLOCK_JID: process.env.BLOCK_JID || "120363365489430422@g.us",
+SESSION_ID: process.env.SESSION_ID || "", //your session id
+MV_SEND_JID: process.env.MV_SEND_JID || "", //default movie send group
+MODE: process.env.MODE || "public", //private or inbox or groups
+AUTO_AI: process.env.AUTO_AI || "on", //on or off
+AUTO_AI_JID: process.env.AUTO_AI_JID || "", //auto ai working group
+BLOCK_JID: process.env.BLOCK_JID || "", //block jids
 };
